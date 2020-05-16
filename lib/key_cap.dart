7fdd12bb4b56e4af394c37f7ac63b6ff20efe8d5
Carlo Colombo <carlo.colombo@gmx.com>
2020-05-16 21:39:30 +0100

    Adding feedback on keycap
    - vibration on tap
    - background change

--- lib/key_cap.dart
+++ lib/key_cap.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:vibration/vibration.dart';
 
-class KeyCap extends StatelessWidget {
+//class KeyCap extends StatefulWidget {
+//  @override
+//  State<StatefulWidget> createState() {
+//    // TODO: implement createState
+//    throw UnimplementedError();
+//  }
+//
+//}
+
+class KeyCap extends StatefulWidget {
   const KeyCap(
       {Key key, @required this.child, @required this.onTap, this.border = true})
       : super(key: key);
@@ -10,16 +20,29 @@ class KeyCap extends StatelessWidget {
   final bool border;
 
   @override
+  _KeyCapState createState() => _KeyCapState();
+}
+
+class _KeyCapState extends State<KeyCap> {
+  bool pressed = false;
+
+  @override
   Widget build(BuildContext context) {
     return GestureDetector(
-        onTap: onTap,
+        onTapDown: (_) {
+          setState(() => pressed = true);
+          return Vibration.vibrate(duration: 50);
+        },
+        onTapUp: (_) => setState(() => pressed = false),
+        onTap: widget.onTap,
         child: Container(
-          child: Center(child: child),
+          child: Center(child: widget.child),
           margin: EdgeInsets.all(4),
           height: 52,
           width: 64,
           decoration: BoxDecoration(
-              border: border ? Border.all(color: Colors.grey) : null,
+              color: pressed ? Theme.of(context).accentColor : Colors.transparent,
+              border: widget.border ? Border.all(color: Colors.grey) : null,
               borderRadius: BorderRadius.circular(5)),
         ));
   }
