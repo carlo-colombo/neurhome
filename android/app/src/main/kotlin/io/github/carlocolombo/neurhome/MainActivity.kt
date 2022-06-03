9fc10c748befec335812e585991a4b4407d1366d
Carlo Colombo <carlo.colombo162@gmail.com>
2022-06-03 22:34:13 +0200

    progress

--- android/app/src/main/kotlin/io/github/carlocolombo/neurhome/MainActivity.kt
+++ android/app/src/main/kotlin/io/github/carlocolombo/neurhome/MainActivity.kt
@@ -47,22 +47,6 @@ class MainActivity : FlutterActivity() {
             }
     }
 
-    override fun onResume() {
-        super.onResume()
-        if (BuildConfig.DEBUG) { // don't even consider it otherwise
-            if (Debug.isDebuggerConnected()) {
-                Log.d(
-                    "SCREEN",
-                    "Keeping screen on for debugging, detach debugger and force an onResume to turn it off."
-                )
-                window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-            } else {
-                window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-                Log.d("SCREEN", "Keeping screen on for debugging is now deactivated.")
-            }
-        }
-    }
-
     private fun listTopApps(call: MethodCall, result: MethodChannel.Result) {
         var count = call.argument<Int>("count") ?: 6
         var i = 0
