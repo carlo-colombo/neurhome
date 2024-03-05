422042fc5b8ea6abe5e8812a2cd2a6fb42a5a4aa
Carlo Colombo <carlo.colombo@gmx.com>
2024-03-05 21:41:13 +0100

    showing multi day calendar events with an alternative background

--- app/src/main/java/ovh/litapp/neurhome3/ui/components/Calendar.kt
+++ app/src/main/java/ovh/litapp/neurhome3/ui/components/Calendar.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -27,12 +28,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
-import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import ovh.litapp.neurhome3.data.Event
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
-@OptIn(ExperimentalPermissionsApi::class)
 @Composable
 @Preview
 fun Calendar(
@@ -60,56 +59,69 @@ fun CalendarItem(
     @PreviewParameter(provider = SampleEventProvider::class) event: Event,
     openEvent: (Event) -> Unit = { }
 ) {
+    val multi = event.end != null && event.dtStart.dayOfYear != event.end.dayOfYear && !event.allDay
+    val bgColor = if (multi) MaterialTheme.colorScheme.primary else Color.Transparent
+    val color =
+        if (multi) MaterialTheme.colorScheme.inversePrimary else MaterialTheme.colorScheme.onSecondaryContainer
     Row(horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier
-            .fillMaxWidth()
-            .clickable {
-                openEvent(event)
-            }) {
+        modifier = Modifier.clickable { openEvent(event) }) {
         Row(
             modifier = Modifier
-                .fillMaxWidth(0.30f)
-                .height(IntrinsicSize.Min)
-                .width(IntrinsicSize.Max)
-                .padding(PaddingValues(end = 10.dp))
+                .background(bgColor)
+                .fillMaxWidth()
         ) {
-            Box(
-                modifier = Modifier
-                    .width(2.dp)
-                    .fillMaxHeight()
-                    .clip(RectangleShape)
-                    .background(event.color)
-            )
-            Spacer(modifier = Modifier.width(2.dp))
             Row(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(PaddingValues(start = 2.dp)),
-                horizontalArrangement = Arrangement.SpaceBetween
+                    .fillMaxWidth(0.30f)
+                    .height(IntrinsicSize.Min)
+                    .width(IntrinsicSize.Max)
+                    .padding(PaddingValues(end = 10.dp))
             ) {
-                Text(text = event.dtStart.format(DateTimeFormatter.ofPattern("dd/MM")))
-                Text(
-                    if (event.allDay) "-" else event.dtStart.format(
-                        DateTimeFormatter.ofPattern("HH:mm")
-
-                    )
+                Box(
+                    modifier = Modifier
+                        .width(3.dp)
+                        .fillMaxHeight()
+                        .clip(RectangleShape)
+                        .background(event.color)
                 )
+                Spacer(modifier = Modifier.width(2.dp))
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(PaddingValues(start = 2.dp)),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        text = event.dtStart.format(DateTimeFormatter.ofPattern("dd/MM")),
+                        color = color
+                    )
+                    Text(
+                        if (event.allDay) "-" else event.dtStart.format(
+                            DateTimeFormatter.ofPattern("HH:mm")
+                        ), color = color
+                    )
+                }
             }
+            Text(
+                event.title, maxLines = 1, overflow = TextOverflow.Ellipsis, color = color
+            )
         }
-        Text(
-            event.title, maxLines = 1, overflow = TextOverflow.Ellipsis,
-        )
     }
 }
 
 class SampleEventProvider : PreviewParameterProvider<Event> {
     override val values = sequenceOf(
-        Event("asdas", dtStart = LocalDateTime.now(), allDay = true, color = Color.Magenta),
-        Event("Other event", dtStart = LocalDateTime.now()),
         Event(
+            "asdas", dtStart = LocalDateTime.now(), end = null, allDay = true, color = Color.Magenta
+        ), Event("Other event", dtStart = LocalDateTime.now(), end = null), Event(
             "Other event p eventeventeventeventeventeventeventevent",
             dtStart = LocalDateTime.now(),
+            null
+        ), Event(
+            "multi day event",
+            dtStart = LocalDateTime.now().minusDays(2),
+            end = LocalDateTime.now().plusDays(2)
         )
     )
 }
@@ -119,20 +131,22 @@ class SampleEventsProvider : PreviewParameterProvider<List<Event>> {
     override val values = sequenceOf(
         listOf(
             Event(
-                "Other event",
-                dtStart = LocalDateTime.parse("2007-11-11T11:11:11"),
+                "Other event", dtStart = LocalDateTime.parse("2007-11-11T11:11:11"), null
             ),
-            Event("asdas", dtStart = LocalDateTime.MAX, allDay = true, color = Color.Cyan),
+            Event("asdas", dtStart = LocalDateTime.MAX, null, allDay = true, color = Color.Cyan),
             Event(
-                "sOther event p eventeventeventeventeventeventeventevent", allDay = true,
+                "multi sOther event p eventeventeventeventeventeventeventevent",
+                allDay = true,
                 dtStart = LocalDateTime.of(2020, 10, 10, 20, 10),
+                end = LocalDateTime.now()
             ),
             Event(
                 "Other event p eventeventeventeventeventeventeventevent",
                 dtStart = LocalDateTime.now(),
+                null
             ),
             Event(
-                "Other events", dtStart = LocalDateTime.now(),
+                "Other events", dtStart = LocalDateTime.now(), null
             )
         )
     )
